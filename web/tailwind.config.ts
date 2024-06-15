174ec107a15a28f5f110d85c0c7489345c392aa1
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2024-06-15 20:43:24 -0300

    Added custom styles using the Tailwind CSS plugin.
    Configured Tailwind CSS to increase its functionality with a custom plugin that defines styles for interactive elements. These changes, which follow established style guidelines, improve the consistency and design of interactive elements throughout the application.

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -8,8 +9,108 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        roboto: ['var(--font-roboto)'],
+        manrope: ['var(--font-manrope)']
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, theme }) {
+      const hoverDelay = theme('transitionDuration.300', '300ms');
+
+      addUtilities({
+        '.bg': {
+          backgroundColor: '#191D1F',
+        },
+        '.btn-submit': {
+          width: '105px',
+          height: '40px',
+          borderWidth: '1px',
+          borderRadius: '8px',
+          backgroundColor: '#FFFFFF',
+          borderColor: '#FFFFFF',
+          color: '#000000',
+          fontSize: '14px',
+          fontWeight: '700',
+          transition: `background-color ${hoverDelay} ease, border-color ${hoverDelay} ease, color ${hoverDelay} ease`,
+          '&:hover': {
+            backgroundColor: '#F27777',
+            borderColor: '#F27777',
+            color: '#FFFFFF',
+          },
+        },
+        '.btn-close': {
+          width: '105px',
+          height: '40px',
+          borderWidth: '1px',
+          borderRadius: '8px',
+          backgroundColor: 'transparent',
+          borderColor: '#A3A3A3',
+          color: '#A3A3A3',
+          fontSize: '14px',
+          fontWeight: '700',
+          transition: `border-color ${hoverDelay} ease, color ${hoverDelay} ease`,
+          '&:hover': {
+            borderColor: '#D4D4D4',
+            color: '#D4D4D4',
+          },
+        },
+        '.btn-cancel': {
+          width: '160px',
+          height: '40px',
+          borderWidth: '1px',
+          borderRadius: '8px',
+          backgroundColor: 'transparent',
+          borderColor: '#F27777',
+          color: '#F27777',
+          fontSize: '14px',
+          fontWeight: '700',
+          transition: `border-color ${hoverDelay} ease, color ${hoverDelay} ease`,
+          '&:hover': {
+            backgroundColor: 'transparent',
+            borderColor: '#E53939',
+            color: '#E53939',
+          },
+        },
+        '.btn-tryAgain': {
+          width: '181px',
+          height: '40px',
+          borderWidth: '1px',
+          borderRadius: '8px',
+          backgroundColor: '#FFFFFF',
+          borderColor: '#FFFFFF',
+          color: '#000000',
+          fontSize: '14px',
+          fontWeight: '700',
+          transition: `background-color ${hoverDelay} ease, border-color ${hoverDelay} ease, color ${hoverDelay} ease`,
+          '&:hover': {
+            backgroundColor: '#F27777',
+            borderColor: '#F27777',
+            color: '#D4D4D4',
+          },
+        },
+        '.btn-topBar': {
+          width: '132px',
+          height: '48px',
+          borderWidth: '1px',
+          borderRadius: '50px',
+          backgroundColor: 'transparent',
+          borderColor: '#E53939',
+          color: '#FFFFFF',
+          fontSize: '18px',
+          fontWeight: '600',
+          transition: `background-color ${hoverDelay} ease, border-color ${hoverDelay} ease, color ${hoverDelay} ease`,
+          '&:hover': {
+            backgroundColor: '#E53939',
+            borderColor: '#E53939',
+            color: '#D4D4D4',
+          },
+        },
+      });
+    }),
+  ],
 };
+
 export default config;
