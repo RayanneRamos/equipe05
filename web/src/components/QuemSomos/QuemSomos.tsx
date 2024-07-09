9f6303c250fcc1dc534f52731b9e8b0b449b698f
dsouloficial <denilsonbnascimento07@gmail.com>
2024-07-09 00:14:44 -0300

    feat: Added fuction QuemSomos

---
+++ web/src/components/QuemSomos/QuemSomos.tsx
@@ -0,0 +1,13 @@
+import * as C from "./Containers"
+
+export default function QuemSomos() {
+  return(
+   <C.Container>
+    <C.ContainerQuemSomos>
+        <C.ContainerLayout>
+            Ola
+        </C.ContainerLayout>
+    </C.ContainerQuemSomos>
+   </C.Container>
+  )
+}
