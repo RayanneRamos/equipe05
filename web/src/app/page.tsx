53faaba446c341a82e94473c339ddad7ec9bbbb3
dsouloficial <denilsonbnascimento07@gmail.com>
2024-07-09 00:17:30 -0300

    feat: Added import fuction QuemSomos to page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import QuemSomos from "@/components/QuemSomos/QuemSomos";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <QuemSomos/>
     </>
   );
 }
