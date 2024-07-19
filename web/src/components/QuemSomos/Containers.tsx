7c7fd1204b72eb49b9e66a55df28d5568d4631f7
Denilson Bernardo <denilsonbnascimento07@gmail.com>
2024-07-18 22:44:13 -0300

    feat: QuemSomos

--- web/src/components/QuemSomos/Containers.tsx
+++ web/src/components/QuemSomos/Containers.tsx
@@ -4,30 +4,25 @@ type Props = {
   children: ReactNode
 }
 
-function Container({ children }: Props) {
+function ContainerText({ children }: Props) {
   return(
-    <section className="containerText xl:w-[151.2rem] border">
-      {children}
-    </section>
+     <section className="w-[527px] xl:py-0 xl:px-16 mr-16">
+       {children}
+     </section>
+  
   )
 }
-function ContainerQuemSomos({ children }: Props) {
+
+function ContainerImage({ children }: Props) {
   return(
-    <section className="containerText xl:w-full xl:h-[75.5rem] border">
-      {children}
-    </section>
-  )
-}
-function ContainerLayout({ children }: Props) {
-  return(
-    <section className="containerText xl:w-full xl:h-[89.008rem] border">
+    <section>
       {children}
     </section>
   )
 }
 
 export {
-  Container,
-  ContainerQuemSomos,
-  ContainerLayout,
+ContainerText,
+ContainerImage,
+
 }
--- web/src/components/QuemSomos/QuemSomos.tsx
+++ web/src/components/QuemSomos/QuemSomos.tsx
@@ -1,13 +1,17 @@
 import * as C from "./Containers"
+import * as E from "./Elements"
 
 export default function QuemSomos() {
   return(
-   <C.Container>
-    <C.ContainerQuemSomos>
-        <C.ContainerLayout>
-            Ola
-        </C.ContainerLayout>
-    </C.ContainerQuemSomos>
-   </C.Container>
+    <section className=" flex xl:flex-row xl:py-[112px] xl:px-[128px]">
+       <C.ContainerText>
+          <E.titulo/>
+          <E.descricao/>
+          <E.acreditamos/>
+       </C.ContainerText>
+       <C.ContainerImage>
+          <E.elementVectorImage/>
+       </C.ContainerImage>
+    </section>
   )
 }
