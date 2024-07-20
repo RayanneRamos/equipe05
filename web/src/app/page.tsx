6a6ac87d5f0382adac708a3976ed652d78801698
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-07-20 03:04:11 +0000

    feat: adicionando componente na home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import NossaEquipe from "@/components/NossaEquipe/NossaEquipe";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <NossaEquipe />
     </>
   );
 }
