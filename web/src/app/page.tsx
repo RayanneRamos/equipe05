6d77c4f4cd7507a1d30d35afd003dfd4584aab0f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-08-08 18:41:39 -0300

    :sparkles: feat: import component footer

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/HeroSection/HeroSection";
 import NossaEquipe from "@/components/NossaEquipe/NossaEquipe";
 import Beneficios from "@/components/Beneficios/Beneficios";
 import Image from "next/image";
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
