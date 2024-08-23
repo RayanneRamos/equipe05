5e2609dec95099e6d7d8b2baace8a0626e1fd0b1
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2024-08-23 16:10:59 -0300

    feat: Added Containers and Elements in Comunidade.tsx

---
+++ web/src/components/Comunidade.tsx
@@ -0,0 +1,28 @@
+import * as C from "./Containers"
+import * as E from "./Elements"
+
+export default function Comunidade() {
+    return (
+        <C.ContainerSection>
+            <C.DiscordContainer>
+                <C.ShapeColorContainer>
+                    <C.InnerContainer>
+                        <E.LogoDiscord />
+                        <C.TextContainer>
+                            <E.Title />
+                            <E.Description />
+                        </C.TextContainer>
+                        <E.Button />
+                    </C.InnerContainer>
+                    <C.ShapeContainer>
+                        <E.StarLeft />
+                        <E.StarRight />
+                    </C.ShapeContainer>
+                    <E.Circle1 />
+                    <E.Circle2 />
+                    <E.Circle3 />
+                </C.ShapeColorContainer>
+            </C.DiscordContainer>
+        </C.ContainerSection>
+    )
+}
