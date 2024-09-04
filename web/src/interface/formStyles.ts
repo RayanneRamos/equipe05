a6b6a2ca28fdcc84d1941edfe7d7cae4c896269f
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2024-09-04 04:36:29 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto para manter a organização e atualização contínua

--- web/src/interface/formStyles.ts
+++
@@ -1,9 +0,0 @@
-export interface FormFieldProps {
-    id: string;
-    label: string;
-    type: 'text' | 'tel' | 'email' | 'date' | 'select';
-    options?: { value: string; label: string }[];
-    value?: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
-    placeholder?: string;
-}
