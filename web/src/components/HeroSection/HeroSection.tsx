d328e9dd838097d0ec31e1f009160c69d95ed85c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-25 15:55:48 -0300

    :sparkles: feat: implementing modal functionality

--- web/src/components/HeroSection/HeroSection.tsx
+++ web/src/components/HeroSection/HeroSection.tsx
@@ -1,21 +1,30 @@
-import * as C from "./Containers"
-import * as E from "./Elements"
+"use client";
+import { useToggle } from "@/hooks/useToggle";
+import * as C from "./Containers";
+import * as E from "./Elements";
+import ModalForm from "../modal/ModalForm";
 
 export default function HeroSection() {
-  return(
+  const { isModalFormOpen, handleModalFormClose, handleModalFormOpen } =
+    useToggle();
+
+  return (
     <C.ContainerHeroSection>
-       <C.ContainerText>
-          <E.titulo/>
-          <E.descricao/>
-          <C.ContainerButtons>
-            <E.buttonParticipe/>
-            <E.buttonSaibaMais/>
-          </C.ContainerButtons>
-        <E.comunidade/>
-       </C.ContainerText>
-       <C.ContainerImage>
-          <E.mapaMundi/>
-       </C.ContainerImage>
+      <C.ContainerText>
+        <E.titulo />
+        <E.descricao />
+        <C.ContainerButtons>
+          <E.buttonParticipe handleModalFormOpen={handleModalFormOpen} />
+          <E.buttonSaibaMais />
+        </C.ContainerButtons>
+        <E.comunidade />
+      </C.ContainerText>
+      <C.ContainerImage>
+        <E.mapaMundi />
+      </C.ContainerImage>
+      {isModalFormOpen && (
+        <ModalForm handleModalFormClose={handleModalFormClose} />
+      )}
     </C.ContainerHeroSection>
-  )
+  );
 }
